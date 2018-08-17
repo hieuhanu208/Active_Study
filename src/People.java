ceec38a374ed9f3257163dfc1b49590dd35cfe6d
Rice <hieuhanufit@fabbi.com.vn>
2018-08-17 10:51:53 +0700

    Editing code for Shopping funtion in People class of Unit 1

--- src/People.java
+++ src/People.java
@@ -9,7 +9,7 @@ public class People{
     double weight;
     double distance;
     double balance;
-
+    
     public static void main(String[] args) {
         People ppl = new People();
         Scanner sc = new Scanner(System.in);
@@ -29,14 +29,12 @@ public class People{
         ppl.eating();
         ppl.working();
         ppl.shopping();
-
-
+        
+        
     }
-
-    public People() {
-        //Constructors
-    }
-
+    
+    
+    
     public void running(){
         this.setDistance(distance+2);
         this.setWeight(weight-0.1);
@@ -45,93 +43,93 @@ public class People{
         System.out.println("Quãng đường mới sau khi chạy là :"+ this.getDistance());
         System.out.println("Cân nặng mới sau khi chạy là:"+this.getWeight());
     }
-
+    
     public void eating(){
         this.setBalance(balance-100);
         this.setWeight(weight+0.2);
         System.out.println("Số tiền còn lạilà sau khi ăn là:"+ this.getBalance());
         System.out.println("Cân nặng mới sau khi ăn là :"+this.getWeight());
     }
-
+    
     public void working(){
         this.setBalance(balance+500);
         System.out.println("Tổng số tiền dư hiện tại sau khi làm việc là: "+ this.getBalance());
     }
-
+    
     public void shopping(){
-        this.setBalance(balance-800);
-        if(this.getBalance() <0){
+        this.setBalance(balance);
+        if(this.getBalance() <800){
             System.out.println("HẾT TIỀN ");
         }else {
-            System.out.println("Tổng số tiền dư hiện tại sau khi chi tiêu là: "+ this.getBalance());
+            System.out.println("Tổng số tiền dư hiện tại sau khi chi tiêu là: "+ (this.getBalance() -800));
         }
-
+        
     }
-
-
+    
+    
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
-
+    
     public int getDateOfBirth() {
         return dateOfBirth;
     }
-
+    
     public void setDateOfBirth(int dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
-
+    
     public int getMonthOfBirth() {
         return monthOfBirth;
     }
-
+    
     public void setMonthOfBirth(int monthOfBirth) {
         this.monthOfBirth = monthOfBirth;
     }
-
+    
     public int getYearOfBirth() {
         return yearOfBirth;
     }
-
+    
     public void setYearOfBirth(int yearOfBirth) {
         this.yearOfBirth = yearOfBirth;
     }
-
+    
     public double getHeight() {
         return height;
     }
-
+    
     public void setHeight(double height) {
         this.height = height;
     }
-
+    
     public double getWeight() {
         return weight;
     }
-
+    
     public void setWeight(double weight) {
         this.weight = weight;
     }
-
+    
     public double getDistance() {
         return distance;
     }
-
+    
     public void setDistance(double distance) {
         this.distance = distance;
     }
-
+    
     public double getBalance() {
         return balance;
     }
-
+    
     public void setBalance(double balance) {
         this.balance = balance;
     }
-
-
+    
 }
+
